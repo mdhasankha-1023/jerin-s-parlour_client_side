6a6c19f75b6a5e40cedc618178060c86a56e191c
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-05-26 01:56:58 +0600

    Jerins Parlour client side project setup complited

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='text-center text-3xl'>
+      This is jerin&rsquo;s parlour client
+    </div>
+  </React.StrictMode>,
+)
