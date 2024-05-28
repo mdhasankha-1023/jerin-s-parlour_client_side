c1814e53ee1cbee32a73cc449d723a83a36ae8a7
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-05-28 18:57:17 +0600

    ADD: Banner component added

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,9 +1,11 @@
+import Banner from "../../Components/Banner/Banner";
 import NewProducts from "../../Components/NewProducts/NewProducts";
 
 
 export default function Main() {
   return (
     <div>
+      <Banner></Banner>
       <NewProducts></NewProducts>
     </div>
   )
