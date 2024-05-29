f8a380226eb62f28f2cd2338a05bfc15797170ee
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-05-29 16:22:42 +0600

    Modify: About  page modify

--- src/Pages/NormalUserPages/About.jsx
+++ src/Pages/NormalUserPages/About.jsx
@@ -1,6 +1,31 @@
+import PrimaryTitle from "../../Components/uI/PrimaryTitle";
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="xl:w-4/5 mx-auto">
+      <PrimaryTitle text1={'About Our'} text2={'Mission'} style={'text-center'}/>
+      <div className="mt-12 mb-16">
+        <p className="text-justify text-xl">
+          <h1 className="text-2xl font-bold my-4">Our Mission</h1>
+          At Jerin&#39;s Parlour, our mission is to create a serene and luxurious haven where beauty and well-being flourish. We are committed to offering an unparalleled experience, where every client is treated with the utmost care and respect. Our goal is to make each visit an indulgent escape from the stresses of daily life, leaving you feeling rejuvenated and radiant.
+
+          <h1 className="text-2xl font-bold my-4">Our Best Facilities</h1>
+
+          We believe that the environment plays a crucial role in your overall experience. That’s why Jerin&#39;s Parlour is designed to be a sanctuary of tranquility and elegance. Our state-of-the-art facilities are equipped with the latest technology and amenities to ensure the highest standards of hygiene and comfort. From our relaxing lounges to our private treatment rooms, every space is thoughtfully curated to enhance your sense of well-being.
+
+          <h1 className="text-2xl font-bold my-4">Our Services</h1>
+
+          At Jerin&#39;s Parlour, we offer a comprehensive range of services tailored to meet your unique needs. Whether you seek revitalizing facials, meticulous manicures, soothing massages, or stylish haircuts, our extensive menu of treatments is designed to enhance your natural beauty. We use only the highest quality products, chosen for their efficacy and safety, to ensure that you receive the best possible care.
+
+          <h1 className="text-2xl font-bold my-4">Our Team</h1>
+
+          Our team of highly skilled professionals is the heart of Jerin&#39;s Parlour. Each member is passionate about their craft and dedicated to delivering exceptional service. Continual education and training ensure that our staff stays at the forefront of industry trends and techniques, providing you with innovative and personalized treatments. We are here to listen, advise, and deliver results that exceed your expectations.
+
+          <h1 className="text-2xl font-bold my-4">Our Commitment to You</h1>
+
+          Ultimately, our mission is to empower you to look and feel your best. At Jerin&#39;s Parlour, your beauty and well-being are our top priorities. We strive to create a warm and inviting atmosphere where you can relax and feel at home. Every detail, from the ambiance of our salon to the personalized attention you receive, is thoughtfully curated to provide an exceptional experience. Your satisfaction is our greatest reward, and we look forward to welcoming you to our sanctuary of beauty and relaxation.
+        </p>
+      </div>
+    </div>
   )
 }
